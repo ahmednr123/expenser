0bb3aab920d72d28a613fb827a037ff9f63103fa
ahmed.n <ahmed.n@zohocorp.com>
2020-10-23 00:48:00 +0530

    added button to create expense

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import ExpenseDataBlock from '../../components/ExpenseDataBlock';
 
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 class Home extends React.Component {
    constructor (props) {
       super(props);
@@ -10,7 +13,10 @@ class Home extends React.Component {
    render() {
       return (
          <div>
-            <h1>Home</h1>
+            <div style={{display:"flex", justifyContent:"space-between"}}>
+               <h1>Expenses</h1>
+               <div><FontAwesomeIcon icon={faPlus} /></div>
+            </div>
             <ExpenseDataBlock data={[{name:"Rent",expense:"8500",type:"debit"}, {name:"Rent",expense:"8500",type:"credit"}]} date="Thursday, 22nd July 2020"/>
             <ExpenseDataBlock data={[{name:"Rent",expense:"8500",type:"debit"}]} date="Saturday, 26th July 2020"/>
          </div>
