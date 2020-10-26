e2a0ece7a4c5d312d29462bfe498be6e83056a27
ahmed.n <ahmed.n@zohocorp.com>
2020-10-27 00:16:16 +0530

    updated tag routes

--- route/Tag.js
+++ route/Tag.js
@@ -1,25 +1,38 @@
 import Express from 'express';
 import {UserAuth} from '../module/Middleware.js';
 
+import TagModel from '../model/Tag.js';
 import ModelController from '../db/controller/Model.js';
+import Tag from '../model/Tag.js';
 
 const Router = Express.Router();
 Router.use(UserAuth);
 
-Router.get('/', () => {
-    
+Router.get('/', (req, res) => {
+    res.end(ModelController.getTags(req.session.userId, null, 10, req.body.offset));
 });
 
-Router.post('/', () => {
-    
+Router.post('/', (req, res) => {
+    let tag = {};
+    tag[TagModel.Column.Name] = req.body.name;
+    tag[TagModel.Column.Color] = req.body.color;
+    ModelController.submitTag(req.session.userId, tag, false, (id) => {
+        res.end(id);
+    });
 });
 
 Router.get('/:tagId', () => {
-    
+    res.end(ModelController.getTags(req.session.userId, req.params.tagId, null, null));
 });
 
 Router.put('/:tagId', () => {
-    
+    let tag = {};
+    tag[TagModel.Column.ID] = req.body.id;
+    tag[TagModel.Column.Name] = req.body.name;
+    tag[TagModel.Column.Color] = req.body.color;
+    ModelController.submitTag(req.session.userId, tag, true, (id) => {
+        res.end(id);
+    });
 });
 
 export default Router;
